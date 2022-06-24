3939a4d81f6a9640e29b5c311bde2272fc207463
Boyd <bebsworthy@gmail.com>
2022-06-24 10:39:57 +0200

    fixup loader

--- README.md
+++ README.md
@@ -7,34 +7,46 @@ First, set your BD in .env.local where {DBNAME}:
 ```bash
 MONGODB_URI=mongodb+srv://mamadou:codedb0@cluster0.rub2l.mongodb.net/{DBNAME}?retryWrites=true&w=majority
 MONGODB_DB={DBNAME}
-
 ```
 
-
-Second, install dependence &  run the development server:
+Example:
 
 ```bash
-npm install 
+MONGODB_URI=mongodb+srv://mamadou:codedb0@cluster0.rub2l.mongodb.net/MAMADOU?retryWrites=true&w=majority
+MONGODB_DB=MAMADOU
+```
+
+Second, install dependence & run the development server:
+
+```bash
+npm install
 
 npm run dev
 # or
 yarn install
 
 yarn dev
-
-```
-Third , init BD in that order
-
-```bash
-localhost/3000/api/insurer/init
-localhost/3000/api/term/init
-localhost/3000/api/finalTerms/init
 ```
 
+# Initialize the database
+
+Visit the following url once.
+
+http://localhost:3000/api/initData
+
+You should see the following response indicating the database was populated with default values
+
+```
+{"status":200,"data":{"reinsurer":[{"_id":"62b5776ea7970dce0417c3af","name":"Reinsurer 1","email":"reinsurer1@gmail.com","password":"123","type":"REINSURER","__v":0},{"_id":"62b5776ea7970dce0417c3b1","name":"Reinsurer 2","email":"reinsurer2@gmail.com","password":"123","type":"REINSURER","__v":0},{"_id":"62b5776ea7970dce0417c3b3","name":"Reinsurer 3","email":"reinsurer3@gmail.com","password":"123","type":"REINSURER","__v":0},{"_id":"62b5776ea7970dce0417c3b5","name":"Reinsurer 4","email":"reinsurer4@gmail.com","password":"123","type":"REINSURER","__v":0}],"termList":[{"_id":"62b5776fa7970dce0417c3b8","reinsurer":{"_id":"62b5776ea7970dce0417c3af","name":"Reinsurer 1","email":"reinsurer1@gmail.com"},"share":30,"__v":0},{"_id":"62b5776fa7970dce0417c3ba","reinsurer":{"_id":"62b5776ea7970dce0417c3b1","name":"Reinsurer 2","email":"reinsurer2@gmail.com"},"share":30,"__v":0},{"_id":"62b5776fa7970dce0417c3bc","reinsurer":{"_id":"62b5776ea7970dce0417c3b3","name":"Reinsurer 3","email":"reinsurer3@gmail.com"},"share":30,"__v":0},{"_id":"62b5776fa7970dce0417c3be","reinsurer":{"_id":"62b5776ea7970dce0417c3b5","name":"Reinsurer 4","email":"reinsurer4@gmail.com"},"share":30,"__v":0}],"finalTerms":{"price":10,"terms":["62b5776fa7970dce0417c3b8","62b5776fa7970dce0417c3ba","62b5776fa7970dce0417c3bc","62b5776fa7970dce0417c3be"],"_id":"62b5776fa7970dce0417c3c3","__v":0}}}
+```
+
+# Open the app
+
+http://localhost:3000
 
 connect insurer credentials: ` username: insurer@gmail.com, password: 123`
-connect reinsurer credentials: 
-`username: reinsurer@gmail.com, password: 123` 
+connect reinsurer credentials:
+`username: reinsurer@gmail.com, password: 123`
 
 `username: reinsurer2@gmail.com, password: 123`
 
@@ -42,13 +54,9 @@ connect reinsurer credentials:
 
 `username: reinsurer4@gmail.com, password: 123`
 
-
-
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+# Cypress e2e
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
+Cypress e2e framework is installed in the /cypress directory.
+See cypress documentaiton and package.json for usage.
--- pages/api/initData/index.ts
+++ pages/api/initData/index.ts
@@ -1,87 +1,93 @@
 // import connectMongo from '../../../middleware/connectMongo';
-import connectToDatabase from '../../../middleware/database'
-import Insurer from '../../../models/insurer'
-import Term from '../../../models/term'
-import {NextApiRequest, NextApiResponse} from 'next'
-import Final_terms from '../../../models/final_terms'
+import connectToDatabase from "../../../middleware/database";
+import Insurer from "../../../models/insurer";
+import Term from "../../../models/term";
+import { NextApiRequest, NextApiResponse } from "next";
+import Final_terms from "../../../models/final_terms";
 
 /**
  * @param {import('next').NextApiRequest} req
  * @param {import('next').NextApiResponse} res
  */
-export default async function InitData(req : NextApiRequest, res: NextApiResponse) {
-    try {
-        await connectToDatabase();
-         let reinsurerList = [
-             {
-             name: 'Insurer',
-             email: 'insurer@gmail.com',
-             password: '123',
-             type: 'INSURER'
-         },
-             {
-             name: 'Reinsurer 1',
-             email: 'reinsurer1@gmail.com',
-             password: '123',
-             type: 'REINSURER'
-         },
-             {
-             name: 'Reinsurer 2',
-             email: 'reinsurer2@gmail.com',
-             password: '123',
-             type: 'REINSURER'
-         },
-             {
-             name: 'Reinsurer 3',
-             email: 'reinsurer3@gmail.com',
-             password: '123',
-             type: 'REINSURER'
-         },
-             {
-             name: 'Reinsurer 4',
-             email: 'reinsurer4@gmail.com',
-             password: '123',
-             type: 'REINSURER'
-         },
-         ]
+export default async function InitData(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await connectToDatabase();
 
-        const res = await fetch('/api/insurer/init')
-        const res1 = await fetch('/api/term/add')
-        const res2 = await fetch('/api/finalTerms/add')
+    let reinsurerList = [
+      {
+        name: "Insurer",
+        email: "insurer@gmail.com",
+        password: "123",
+        type: "INSURER",
+      },
+      {
+        name: "Reinsurer 1",
+        email: "reinsurer1@gmail.com",
+        password: "123",
+        type: "REINSURER",
+      },
+      {
+        name: "Reinsurer 2",
+        email: "reinsurer2@gmail.com",
+        password: "123",
+        type: "REINSURER",
+      },
+      {
+        name: "Reinsurer 3",
+        email: "reinsurer3@gmail.com",
+        password: "123",
+        type: "REINSURER",
+      },
+      {
+        name: "Reinsurer 4",
+        email: "reinsurer4@gmail.com",
+        password: "123",
+        type: "REINSURER",
+      },
+    ];
 
-        reinsurerList?.map(async (user) => {
-            // termList.push(term)
-            return await Insurer.create({
-                ...user
-            })
-        })
-
-        const reinsurer = await Insurer.find({type: 'REINSURER'}, {}).then(list => list);
-
-       reinsurer?.map(async (user) => {
-           // termList.push(term)
-           return await Term.create({
-               reinsurer: user?._id, // req.body.reinsurer,
-               share: 30, // req.body.share
-           })
-       })
-       const termList =  await Term.find({})
-           .populate('reinsurer', {'name': 1, 'email': 1}, 'Insurer')
-
-
-       const finalTerms = await Final_terms.create({
-           price: 10,
-           terms: await Term.find({}).then((term) => term.map((t) => t._id))
-       });
-
-        
-        res.json({ status: 200, data: {
-            reinsurer,
-            termList,
-            finalTerms
-        } });
-    } catch (error) {
-        console.log(error);
-        res.json({ error });
+    for (let i = 0; i < reinsurerList.length; i++) {
+      await Insurer.create({
+        ...reinsurerList[i],
+      });
     }
+
+    const reinsurer = await Insurer.find({ type: "REINSURER" }, {}).then(
+      (list) => list
+    );
+
+    for (let i = 0; i < reinsurer.length; i++) {
+      // termList.push(term)
+      await Term.create({
+        reinsurer: reinsurer[i]?._id, // req.body.reinsurer,
+        share: 30, // req.body.share
+      });
+    }
+
+    const termList = await Term.find({}).populate(
+      "reinsurer",
+      { name: 1, email: 1 },
+      "Insurer"
+    );
+
+    const finalTerms = await Final_terms.create({
+      price: 10,
+      terms: await Term.find({}).then((term) => term.map((t) => t._id)),
+    });
+
+    res.json({
+      status: 200,
+      data: {
+        reinsurer,
+        termList,
+        finalTerms,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ error });
+  }
 }
